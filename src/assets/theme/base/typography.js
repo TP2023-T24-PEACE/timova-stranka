eb54b40e5c54e6181933fab0e7cbfbe42703270d
Matej Pakán <mattpakan@gmail.com>
2023-10-25 16:27:31 +0200

    Removed components from homepage

--- src/assets/theme/base/typography.js
+++ src/assets/theme/base/typography.js
@@ -28,8 +28,8 @@ import pxToRem from "assets/theme/functions/pxToRem";
 const { dark } = colors;
 
 const baseProperties = {
-  fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
-  fontFamily2: '"Roboto Slab", sans-serif',
+  fontFamily: "Poppins",
+  fontFamily2: "Poppins",
   fontWeightLighter: 100,
   fontWeightLight: 300,
   fontWeightRegular: 400,
